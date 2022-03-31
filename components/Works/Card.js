925a42286888cec8ceee0b725005cb5a8dff1e43
Mohamed Salah <msmido9@gmail.com>
2022-03-31 18:45:17 +0200

    adding js projects my works section

--- components/Works/Card.js
+++ components/Works/Card.js
@@ -2,11 +2,22 @@ import Link from "next/link";
 import Image from "next/image";
 import defaultImage from "../../public/default.jpg";
 
-function Card({ project, cloudinary_image_v, cloudinary_image_name }) {
+function Card({
+	project,
+	cloudinary_image_v,
+	cloudinary_image_name,
+	cardType,
+}) {
 	return (
 		<Link href={project.projectLink}>
 			<a target="_blank">
-				<div className="shadow-lg rounded-lg border-t-4 border-blue-300 p-6">
+				<div
+					className={`shadow-lg rounded-lg ${
+						cardType === "reactjs"
+							? "border-t-4 border-blue-300 p-6"
+							: "border-b-2 border-blue-300 pl-6 p-2"
+					}  `}
+				>
 					<header>
 						<h2 className=" text-blue-500 mb-4 text-sm md:text-base lg:text-3xl text-bolder font-montserrat font-bold">
 							{project.projectName}
@@ -15,17 +26,19 @@ function Card({ project, cloudinary_image_v, cloudinary_image_name }) {
 							<p>{project.projectDesc}</p>
 						</div>
 					</header>
-					<Image
-						src={
-							`https://res.cloudinary.com/dexrp1zlw/image/upload/${cloudinary_image_v}/my-portofolio/works/${cloudinary_image_name}.png`
-								? project.projectScreen.src
-								: defaultImage
-						}
-						layout="responsive"
-						width="1360"
-						height="600"
-						className="rounded-[8px]"
-					/>
+					{cardType == "reactjs" ? (
+						<Image
+							src={
+								`https://res.cloudinary.com/dexrp1zlw/image/upload/${cloudinary_image_v}/my-portofolio/works/${cloudinary_image_name}.png`
+									? project.projectScreen.src
+									: defaultImage
+							}
+							layout="responsive"
+							width="1360"
+							height="600"
+							className="rounded-[8px]"
+						/>
+					) : cardType == "purejs" ? null : null}
 				</div>
 			</a>
 		</Link>
