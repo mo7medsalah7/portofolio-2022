e6c879053b95dcecf861513225ee166aaf640821
mo7medsalah7 <msmido9@gmail.com>
2022-04-12 19:16:35 +0200

    remove tabs section

--- components/Header/HeaderData.js
+++ components/Header/HeaderData.js
@@ -1,38 +1,43 @@
 import Image from "next/image";
-import BackgroundImage from "./mosalah.png";
+import BackgroundImage from "./moSalah.png";
 function HeaderData() {
-	return (
-		<div className=" bg-[#f7c71f]  flex  m-auto">
-			<div className=" flex w-full min-h-screen ">
-				<div className=" m-auto">
-					<div className="flex flex-col">
-						<div className="mb-6">
-							<h1 className="text-4xl md:text-8xl lg:text-9xl font-bold brandName">
-								Mo Salah
-							</h1>
-							<p className="text-base md:text-2xl">
-								Let's collaborate on your next project.
-							</p>
-						</div>
-						<div className="buttons flex gap-[12px]">
-							<div className="basicBtn">
-								<button type="submit">Let's Work</button>
-							</div>
-							<div className="secondBtn">
-								<button className="" type="submit">
-									See My Works
-								</button>
-							</div>
-						</div>
-					</div>
-				</div>
-				{/* Portofolio Image */}
-				<div className="m-auto hidden md:hidden lg:flex">
-					<Image src={BackgroundImage} alt="Picture of Mo Salah" />
-				</div>
-			</div>
-		</div>
-	);
+  return (
+    <div className="  flex  m-auto">
+      <div className=" flex w-full min-h-screen ">
+        <div className=" m-auto">
+          <div className="flex flex-col">
+            <div className="mb-6">
+              <h1 className="text-4xl md:text-8xl lg:text-9xl font-bold brandName">
+                Mo Salah
+              </h1>
+              <p className="text-base md:text-2xl">
+                Let's collaborate on your next project.
+              </p>
+            </div>
+            <div className="buttons flex gap-[12px]">
+              <div className="basicBtn">
+                <button type="submit">Let's Work</button>
+              </div>
+              <div className="secondBtn">
+                <button className="" type="submit">
+                  See My Works
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+        {/* Portofolio Image */}
+        <div className="m-auto hidden md:hidden lg:flex">
+          <Image
+            src={BackgroundImage}
+            className=" rounded-[40px]"
+            alt="Picture of Mo Salah"
+            layout="intrinsic"
+          />
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default HeaderData;
--- pages/index.js
+++ pages/index.js
@@ -5,30 +5,26 @@ import Works from "../components/Works/Works";
 import Tabs from "../components/Tabs";
 
 export default function Home() {
-	return (
-		<div className="">
-			<Head>
-				<title>Front Mo,S </title>
-				<link rel="icon" href="/favicon.ico" />
-				<link rel="preconnect" href="https://fonts.googleapis.com" />
-				<link
-					rel="preconnect"
-					href="https://fonts.gstatic.com"
-					crossorigin
-				/>
-				<link
-					href="https://fonts.googleapis.com/css2?family=Bangers&family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&family=Roboto:wght@100;400&display=swap"
-					rel="stylesheet"
-				/>
-			</Head>
-			<header>
-				<HeaderData />
-			</header>
-			<main className="main-font overflow-hidden">
-				<Skills />
-				<Works />
-				<Tabs />
-			</main>
-		</div>
-	);
+  return (
+    <div className="">
+      <Head>
+        <title>Front Mo,S </title>
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Bangers&family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&family=Roboto:wght@100;400&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
+      <header>
+        <HeaderData />
+      </header>
+      <main className="main-font overflow-hidden">
+        <Skills />
+        <Works />
+        {/* <Tabs /> */}
+      </main>
+    </div>
+  );
 }
