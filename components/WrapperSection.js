686f2487c36bd3e80a0b91cbeae9762c2a471695
mo7medsalah7 <msmido9@gmail.com>
2022-05-16 00:50:05 +0200

    change the page background

--- components/WrapperSection.js
+++ components/WrapperSection.js
@@ -2,7 +2,7 @@ import React, { Children } from "react";
 
 function WrapperSection(props) {
   return (
-    <div className="container main-font lg:w-5/6 m-auto py-5 mt-20 mb-4 ">
+    <div className="container main-font lg:w-5/6 m-auto py-5 sm:mt-10 lg:mt-15 xl:mt-18 mb-4 ">
       {props.children}
     </div>
   );
