a2fde51e8d6dca7e8f7632aeb6fa3f3f3f32050f
mo7medsalah7 <msmido9@gmail.com>
2022-05-24 19:34:03 +0200

    fixing some issues in SEO

--- pages/index.js
+++ pages/index.js
@@ -68,7 +68,7 @@ export default function Home({ title }) {
     </div>
   );
 }
-export async function getServerSideProps(context) {
+export async function getStaticProps(context) {
   return {
     props: {
       title: "Front Mo,S",
