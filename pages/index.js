7bc599bfa539845247b4e267f5fbbe3eb28b6a4a
mo7medsalah7 <msmido9@gmail.com>
2022-05-25 03:25:34 +0200

    test, Head

--- pages/index.js
+++ pages/index.js
@@ -7,6 +7,28 @@ import Works from "../components/Works/Works";
 export default function Home() {
   return (
     <div>
+      <Head>
+        <title>Front Mo,S</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <meta
+          name="og:title"
+          property="og:title"
+          content="Mohamed Salah Portofolio"
+        />
+        <meta name="og:type" property="og:type" content="website" />
+
+        <meta
+          name="og:description"
+          property="og:description"
+          content="Mohamed Salah Portofolio, A Frontend Developer and DevOps Engineer"
+        />
+        <meta
+          name="og:image"
+          property="og:image"
+          content="https://res.cloudinary.com/dexrp1zlw/image/upload/v1653409172/my-portofolio/image-seo.png"
+        />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <header>
         <HeaderData />
       </header>
