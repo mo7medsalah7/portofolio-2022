f831e56bc63850c4f908e243af45dff808d4de05
mo7medsalah7 <msmido9@gmail.com>
2022-08-01 16:41:19 +0200

    editing font family of works card heading #2

--- components/Works/Card.js
+++ components/Works/Card.js
@@ -1,6 +1,6 @@
-import Link from "next/link";
-import Image from "next/image";
-import defaultImage from "../../public/default.jpg";
+import Link from 'next/link';
+import Image from 'next/image';
+import defaultImage from '../../public/default.jpg';
 
 function Card({
   project,
@@ -13,20 +13,20 @@ function Card({
       <a target="_blank">
         <div
           className={`shadow-lg ${
-            cardType === "reactjs"
-              ? "border-t-4 border-blue-300 p-6"
-              : "border-b-2 border-blue-300 pl-6 p-2"
+            cardType === 'reactjs'
+              ? 'border-t-4 border-blue-300 p-6'
+              : 'border-b-2 border-blue-300 pl-6 p-2'
           }  `}
         >
           <header>
-            <h2 className=" text-blue-500 mb-4 text-sm md:text-base lg:text-3xl text-bolder font-montserrat font-bold">
+            <h2 className="works_card_header text-blue-500 mb-4 text-sm md:text-base lg:text-3xl text-bolder font-montserrat font-bold">
               {project.projectName}
             </h2>
             <div className="my-5">
               <p>{project.projectDesc}</p>
             </div>
           </header>
-          {cardType == "reactjs" ? (
+          {cardType == 'reactjs' ? (
             <Image
               src={
                 `https://res.cloudinary.com/dexrp1zlw/image/upload/${cloudinary_image_v}/my-portofolio/works/${cloudinary_image_name}.png`
@@ -39,7 +39,7 @@ function Card({
               className="rounded-[8px]"
               alt={project.screenAlt}
             />
-          ) : cardType == "purejs" ? null : null}
+          ) : cardType == 'purejs' ? null : null}
         </div>
       </a>
     </Link>
