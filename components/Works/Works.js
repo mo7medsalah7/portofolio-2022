1d36f205a5fbc8033bd86c7f625208df8390b832
mo7medsalah7 <msmido9@gmail.com>
2022-08-01 16:28:53 +0200

    adding grocery shop app

--- components/Works/Works.js
+++ components/Works/Works.js
@@ -1,138 +1,148 @@
-import WrapperSection from "../WrapperSection";
-import Card from "./Card";
-import ParadisoScreen from "../../public/paradiso.png";
-import NetflixCloneScreen from "../../public/netflix-clone.png";
-import RealStateScreen from "../../public/real-estate.png";
-import BudgetPlusScreen from "../../public/budgetplus.png";
-import SimpleCalculatorScreen from "../../public/simple-calculator.png";
-import QuizAppScreen from "../../public/QuizApp.png";
-import SearchFilterScreen from "../../public/search-filter.png";
-import FormValidatorScreen from "../../public/form-validator.png";
-import WorksHeader from "./WorksHeader";
-import Item from "../Item";
+import WrapperSection from '../WrapperSection';
+import Card from './Card';
+import ParadisoScreen from '../../public/paradiso.png';
+import NetflixCloneScreen from '../../public/netflix-clone.png';
+import RealStateScreen from '../../public/real-estate.png';
+import BudgetPlusScreen from '../../public/budgetplus.png';
+import GroceryAppScreen from '../../public/GroceryApp.png';
+import SimpleCalculatorScreen from '../../public/simple-calculator.png';
+import QuizAppScreen from '../../public/QuizApp.png';
+import SearchFilterScreen from '../../public/search-filter.png';
+import FormValidatorScreen from '../../public/form-validator.png';
+import WorksHeader from './WorksHeader';
+import Item from '../Item';
 
 const worksDetails = [
   {
-    projectName: "Paradiso",
-    projectLink: "https://paradiso-movies-react.vercel.app/",
-    cloudinary_image_v: "v1648737039",
-    cloudinary_image_name: "paradiso-hero",
-    projectDesc: "app for movies exploring.",
+    projectName: 'Paradiso',
+    projectLink: 'https://paradiso-movies-react.vercel.app/',
+    cloudinary_image_v: 'v1648737039',
+    cloudinary_image_name: 'paradiso-hero',
+    projectDesc: 'app for movies exploring.',
     projectScreen: ParadisoScreen,
-    screenAlt: "website image",
+    screenAlt: 'website image',
   },
 
   {
-    projectName: "Real Estate Explore",
-    projectLink: "https://real-estate-explorer.vercel.app/",
-    projectDesc: "Explore a room !",
-    cloudinary_image_v: "v1648738635",
-    cloudinary_image_name: "real-estate_gcjogr",
+    projectName: 'Real Estate Explore',
+    projectLink: 'https://real-estate-explorer.vercel.app/',
+    projectDesc: 'Explore a room !',
+    cloudinary_image_v: 'v1648738635',
+    cloudinary_image_name: 'real-estate_gcjogr',
     projectScreen: RealStateScreen,
-    screenAlt: "website image",
+    screenAlt: 'website image',
   },
   {
-    projectName: "Budget+",
-    projectLink: "https://budget-traker.vercel.app/",
-    cloudinary_image_v: "v1649784303",
-    cloudinary_image_name: "budgetplus",
-    projectDesc: "Your budget and expenses tracking.",
+    projectName: 'Budget+',
+    projectLink: 'https://budget-traker.vercel.app/',
+    cloudinary_image_v: 'v1649784303',
+    cloudinary_image_name: 'budgetplus',
+    projectDesc: 'Your budget and expenses tracking.',
     projectScreen: BudgetPlusScreen,
-    screenAlt: "website image",
+    screenAlt: 'website image',
   },
   {
-    projectName: "Calculator",
+    projectName: 'Grocery Shop',
+    projectLink: 'https://grocery-bud-one.vercel.app/',
+    cloudinary_image_v: 'v1659364017',
+    cloudinary_image_name: 'grocery-shop',
+    projectDesc: 'Add to your grocery.',
+    projectScreen: GroceryAppScreen,
+    screenAlt: 'website image',
+  },
+  {
+    projectName: 'Calculator',
     projectLink:
-      "https://simple-calculator-j7vtrzcel-mo7medsalah7.vercel.app/  ",
-    cloudinary_image_v: "v1649961056",
-    cloudinary_image_name: "simple-calculator",
-    projectDesc: "A simple calculator.",
+      'https://simple-calculator-j7vtrzcel-mo7medsalah7.vercel.app/  ',
+    cloudinary_image_v: 'v1649961056',
+    cloudinary_image_name: 'simple-calculator',
+    projectDesc: 'A simple calculator.',
     projectScreen: SimpleCalculatorScreen,
-    screenAlt: "website image",
+    screenAlt: 'website image',
   },
   {
-    projectName: "Quiz App",
-    projectLink: "https://quiz-appp.vercel.app/",
-    cloudinary_image_v: "v1653238497",
-    cloudinary_image_name: "QuizApp",
-    projectDesc: "A simple quiz app.",
+    projectName: 'Quiz App',
+    projectLink: 'https://quiz-appp.vercel.app/',
+    cloudinary_image_v: 'v1653238497',
+    cloudinary_image_name: 'QuizApp',
+    projectDesc: 'A simple quiz app.',
     projectScreen: QuizAppScreen,
-    screenAlt: "website image",
+    screenAlt: 'website image',
   },
   {
-    projectName: "Search Filter App",
-    projectLink: "https://search-filter.vercel.app",
-    cloudinary_image_v: "v1653316745",
-    cloudinary_image_name: "search-filter",
-    projectDesc: "Simple Filteration App.",
+    projectName: 'Search Filter App',
+    projectLink: 'https://search-filter.vercel.app',
+    cloudinary_image_v: 'v1653316745',
+    cloudinary_image_name: 'search-filter',
+    projectDesc: 'Simple Filteration App.',
     projectScreen: SearchFilterScreen,
-    screenAlt: "website image",
+    screenAlt: 'website image',
   },
   {
-    projectName: "Form Validator",
-    projectLink: "https://form-validator-u.vercel.app/",
-    cloudinary_image_v: "v1653346332",
-    cloudinary_image_name: "form-validator",
-    projectDesc: "A Form Validator Without Any External Libraries.",
+    projectName: 'Form Validator',
+    projectLink: 'https://form-validator-u.vercel.app/',
+    cloudinary_image_v: 'v1653346332',
+    cloudinary_image_name: 'form-validator',
+    projectDesc: 'A Form Validator Without Any External Libraries.',
     projectScreen: FormValidatorScreen,
-    screenAlt: "website image",
+    screenAlt: 'website image',
   },
   {
-    projectName: "Netflix Clone",
-    projectLink: "https://netflix-clone-phi-ruby.vercel.app/",
-    projectDesc: "A clone for the netflix main header.",
-    cloudinary_image_v: "v1648737590",
-    cloudinary_image_name: "netflix-hero_rvrdfr",
+    projectName: 'Netflix Clone',
+    projectLink: 'https://netflix-clone-phi-ruby.vercel.app/',
+    projectDesc: 'A clone for the netflix main header.',
+    cloudinary_image_v: 'v1648737590',
+    cloudinary_image_name: 'netflix-hero_rvrdfr',
     projectScreen: NetflixCloneScreen,
-    screenAlt: "website image",
+    screenAlt: 'website image',
   },
 ];
 
 const JsWorksDetails = [
   {
-    itemName: "Colour Flipper",
+    itemName: 'Colour Flipper',
     isLink: true,
     itemLink:
-      "https://javascript-projects-salah.netlify.app/color-flipper/index.html",
+      'https://javascript-projects-salah.netlify.app/color-flipper/index.html',
   },
   {
-    itemName: "Counter",
+    itemName: 'Counter',
     isLink: true,
     itemLink:
-      "https://javascript-projects-salah.netlify.app/counter/index.html",
+      'https://javascript-projects-salah.netlify.app/counter/index.html',
   },
   {
-    itemName: "Menu",
+    itemName: 'Menu',
     isLink: true,
-    itemLink: "https://javascript-projects-salah.netlify.app/menu/index.html",
+    itemLink: 'https://javascript-projects-salah.netlify.app/menu/index.html',
   },
   {
-    itemName: "Modal",
+    itemName: 'Modal',
     isLink: true,
-    itemLink: "https://javascript-projects-salah.netlify.app/modal/index.html",
+    itemLink: 'https://javascript-projects-salah.netlify.app/modal/index.html',
   },
   {
-    itemName: "Music Player",
-    isLink: true,
-    itemLink:
-      "https://javascript-projects-salah.netlify.app/music-player/index.html",
-  },
-  {
-    itemName: "Questions",
+    itemName: 'Music Player',
     isLink: true,
     itemLink:
-      "https://javascript-projects-salah.netlify.app/questions/index.html",
+      'https://javascript-projects-salah.netlify.app/music-player/index.html',
   },
   {
-    itemName: "Reviews",
+    itemName: 'Questions',
     isLink: true,
     itemLink:
-      "https://javascript-projects-salah.netlify.app/reviews/index.html",
+      'https://javascript-projects-salah.netlify.app/questions/index.html',
   },
   {
-    itemName: "Scroll",
+    itemName: 'Reviews',
     isLink: true,
-    itemLink: "https://javascript-projects-salah.netlify.app/scroll/index.html",
+    itemLink:
+      'https://javascript-projects-salah.netlify.app/reviews/index.html',
+  },
+  {
+    itemName: 'Scroll',
+    isLink: true,
+    itemLink: 'https://javascript-projects-salah.netlify.app/scroll/index.html',
   },
 ];
 
@@ -148,7 +158,7 @@ function Works() {
       </div>
 
       <div className="text-left ">
-        <WorksHeader title={"React & Nextjs "} logo="nextjs" />
+        <WorksHeader title={'React & Nextjs '} logo="nextjs" />
         <div className="container mx-auto shadow-grey-400">
           <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
             {worksDetails.map((project) => {
@@ -165,7 +175,7 @@ function Works() {
             })}
           </div>
         </div>
-        <WorksHeader title={"Pure Javascript"} logo="js" />
+        <WorksHeader title={'Pure Javascript'} logo="js" />
         <div className="container mx-auto shadow-grey-400">
           <Item items={JsWorksDetails} isLink />
         </div>
