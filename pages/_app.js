77612e7112621f342b07e18c307b69be257d1580
mo7medsalah7 <msmido9@gmail.com>
2023-03-07 11:13:35 +0200

    fixing a typo

--- pages/_app.js
+++ pages/_app.js
@@ -13,7 +13,7 @@ function MyApp({ Component, pageProps }) {
   return (
     <>
       <Head>
-        <title> DevOpes Mo,S </title>
+        <title> DevOps Mo,S </title>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
         <link
